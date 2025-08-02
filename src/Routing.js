df04aef5ceda57b5627f8ef49cbabc3c45594873
Harsh Kumar <harshkumarsonkar55940@gmail.com>
2025-08-02 17:18:00 +0530

    Correct in Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Slide from './Slide';
 import Dashboard from './Dashboard/Dashboard';
 import Teacher from './Dashboard/Teacher/Teacher';
